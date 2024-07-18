5d43caaf9c689c74be72358d5c10e32a3d8d1c3c
ryoupr <ryou120710@gmail.com>
2024-07-18 15:08:31 +0900

    a

---
+++ src/bigint.ts
@@ -0,0 +1,7 @@
+const bignum: bigint = (123n + 456n) * 2n;
+console.log("bignum")
+console.log(bignum);
+
+const bignum2: bigint = 5n / 2n;
+console.log("bignum2 は小数点が丸められてしまう")
+console.log(bignum2)
